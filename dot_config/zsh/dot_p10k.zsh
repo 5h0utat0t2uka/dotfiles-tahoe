1574b21fbf63ff1ffcebc8fb47c907008fe9a600
shouta totsuka <5h0utat0t2uka@gmail.com>
2025-12-28 01:46:40 +0900

    update

--- dot_config/zsh/dot_p10k.zsh
+++ dot_config/zsh/dot_p10k.zsh
@@ -800,7 +800,7 @@
   # Custom icon.
   # typeset -g POWERLEVEL9K_CHEZMOI_SHELL_VISUAL_IDENTIFIER_EXPANSION=''
   # typeset -g POWERLEVEL9K_CHEZMOI_SHELL_VISUAL_IDENTIFIER_EXPANSION=''
-  typeset -g POWERLEVEL9K_CHEZMOI_SHELL_VISUAL_IDENTIFIER_EXPANSION=''
+  typeset -g POWERLEVEL9K_CHEZMOI_SHELL_VISUAL_IDENTIFIER_EXPANSION='󱄅'
 
   ##################################[ disk_usage: disk usage ]##################################
   # Colors for different levels of disk usage.
